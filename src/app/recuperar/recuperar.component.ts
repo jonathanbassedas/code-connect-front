8067fb90c44d1c14cd7b688d0971f79a298196fa
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-04-04 17:18:57 +0200

    Genera nuevo componente: recuperar Para pantalla de recuperar contraseña.

---
+++ src/app/recuperar/recuperar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recuperar',
+  standalone: true,
+  imports: [],
+  templateUrl: './recuperar.component.html',
+  styleUrl: './recuperar.component.css'
+})
+export class RecuperarComponent {
+
+}
